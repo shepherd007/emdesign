bac7195aa02a6eef0b46de76f29734a439165fbc
Grzegorz Suder <AGS004C@motorolasolutions.com>
2016-01-14 14:51:43 +0100

    test_point_multi_touch

--- EmDesign/EmDesign/tests.cpp
+++ EmDesign/EmDesign/tests.cpp
@@ -5,6 +5,8 @@
 #include "ScreenMatrixImpl.h"
 #include "Point.h"
 
+PointObject* calcPoint(const std::vector<int>& v);
+
 class TouchFixture : public ::testing::Test
 {
 protected:
@@ -24,12 +26,23 @@ TEST_F(TouchFixture, test_matrix_report_empty_to_touch)
 	std::vector<int> touched_vec(100);
 
 	touched_vec[0] = 1;
-
 	
-
 	std::string first = (*pMat)(empty_vec);
 	std::string second = (*pMat)(touched_vec);
 
 	EXPECT_EQ(0, first.compare(""));
 	EXPECT_EQ(0, second.compare(""));
+}
+
+
+//
+TEST_F(TouchFixture, test_point_multi_touch)
+{
+	std::vector<int> empty_vec(100);
+	std::vector<int> touched_vec(100);
+
+	touched_vec[4] = 1;
+	touched_vec[21] = 1;
+	PointObject* pt = calcPoint(touched_vec);
+	EXPECT_EQ("D(3,2)", pt->toString());
 }
