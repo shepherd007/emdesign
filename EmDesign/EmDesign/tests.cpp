f5c2b9502e82eaadd991f7ed255b835050ac2ec9
kosa <kosinski.michal@gmail.com>
2016-01-14 15:23:27 +0100

    fixed failing tests

--- EmDesign/EmDesign/tests.cpp
+++ EmDesign/EmDesign/tests.cpp
@@ -19,16 +19,19 @@ protected:
 	std::vector<int> touched_vec1;
 	std::vector<int> touched_vec2;
 
+
+
 	
 	void SetUp() { 
 		pMat = createScreenMatrix();
 		empty_vec = std::vector<int>(100);
 		touched_vec = std::vector<int>(100);
-		touched_vec[0] = 1;
 		touched_vec1 = std::vector<int>(touched_vec);
 		touched_vec2 = std::vector<int>(touched_vec);
 		empty_vec1 = std::vector<int>(empty_vec);
 		empty_vec2 = std::vector<int>(empty_vec);
+
+
 	}
 	void TearDown() {
 		delete pMat;
@@ -64,6 +67,45 @@ TEST_F(TouchFixture, test_matrix_report_empty_to_empty)
 	EXPECT_EQ(0, second.compare(""));
 }
 
+// Sygnalem zakonczenia przetwarzania jest odczyt pustej matrycy po pelnej
+TEST_F(TouchFixture, test_matrix_report_touch_to_empty1)
+{
+	touched_vec[0] = 1;
+	std::string first = (*pMat)(touched_vec);
+	std::string second = (*pMat)(empty_vec);
+	std::string third = (*pMat)(empty_vec);
+	EXPECT_EQ(0, first.compare(""));
+	EXPECT_EQ(0, second.compare("D(1,1)"));
+	EXPECT_EQ(0, third.compare(""));
+}
+
+// Gdy matryca pusta jest poprzedzona wiecej niz jedna matryca pelna, wynik powinien pochodzic z ostatniej pelnej matrycy
+TEST_F(TouchFixture, test_matrix_report_touch_to_empty2)
+{
+	touched_vec1[0] = 0;
+	touched_vec2[0] = 0;
+	touched_vec1[1] = 1;
+	touched_vec2[1] = 1;
+	std::string first = (*pMat)(touched_vec1);
+	std::string second = (*pMat)(touched_vec2);
+	std::string third = (*pMat)(empty_vec);
+
+	EXPECT_EQ(0, first.compare(""));
+	EXPECT_EQ(0, second.compare(""));
+	EXPECT_EQ(0, third.compare("D(2,1)"));
+}
+
+TEST_F(TouchFixture, test_matrix_report_empty_to_touch_to_empty)
+{
+	touched_vec[99] = 1;
+	std::string first = (*pMat)(empty_vec);
+	std::string second = (*pMat)(touched_vec);
+	std::string third = (*pMat)(empty_vec);
+
+	EXPECT_EQ(0, first.compare(""));
+	EXPECT_EQ(0, second.compare(""));
+	EXPECT_EQ(0, third.compare("D(10,10)"));
+}
 
 //
 TEST_F(TouchFixture, test_point_to_string)
